ef199d4fcdd665317772587b830fba697253ee0f
bzhezinskyi <108461894+bzhezinskyi@users.noreply.github.com>
2022-09-16 22:04:23 +0300

    updata

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,26 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const el = galleryItems.map(el => {
+  const item = document.createElement('a');
+  item.classList.add('gallery__item');
+  item.href = el.original;
+
+  const image = document.createElement('img');
+  image.src = el.preview;
+  image.alt = el.description;
+  image.classList.add('gallery__image');
+
+  item.append(image);
+  return item;
+});
+
+gallery.append(...el);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+  caption: 'sss',
+});
+console.log('~ lightbox', lightbox);
+console.log('~ lightbox', lightbox);
